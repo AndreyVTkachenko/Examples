3d32edeaa78fe2a3ef16619abae0c4c7807a21c1
«Andrew» <>
2023-07-13 00:09:24 +0700

    added ? in Example 002 & 005

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Отдельное приветствие для определённого пользователя с помощью условия If
 Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();
 
-if(username.ToLower() == "andrew")
+if(username?.ToLower() == "andrew")
 {
     Console.WriteLine("Ура! Это же Andrew!");
 }
